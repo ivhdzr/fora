c5c8e7c887997d13ca4612ef56a2d09a0bb39a3d
AlexRv95 <114432278+AlexRv95@users.noreply.github.com>
2022-10-02 17:14:19 -0500

    Update bajaSuscripcion.php

--- bajaSuscripcion.php
+++ bajaSuscripcion.php
@@ -13,6 +13,23 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="index.html" class="brand-logo"><img src="img/logo_fora.png" alt="Logo fora" class="nav-img"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.html#conocenos" class="blue-text">Conócenos</a></li>
+        <li><a href="index.html#productos" class="blue-text">Productos</a></li>
+        <li><a href="index.html#contactanos" class="blue-text">Contáctanos</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="index.html#conocenos">Conócenos</a></li>
+        <li><a href="index.html#productos">Productos</a></li>
+        <li><a href="index.html#contactanos">Contáctanos</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger blue-text"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
   
 </body>
 </html>
