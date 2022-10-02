2ac39f3060684a6a791769138acd642f231655b5
AlexRv95 <114432278+AlexRv95@users.noreply.github.com>
2022-10-02 15:20:18 -0500

    Add files via upload

---
+++ suscripcion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <meta name="author" content="Equipo de desarrollo FORA"/>
+  <meta name="description" content="FORA. Soluciones de software eficientes e innovadores.">
+  <title>Suscripción al boletín</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  
+</body>
+</html>
