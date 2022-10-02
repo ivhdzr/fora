9b0c959c975d36aefe0f904731f986462c555172
AlexRv95 <114432278+AlexRv95@users.noreply.github.com>
2022-10-02 15:48:00 -0500

    Update suscripcion.php

--- suscripcion.php
+++ suscripcion.php
@@ -34,6 +34,36 @@
     <a href="/fora">Inicio</a> / <a href="#" class="under-text">Suscripción al boletín</a>
   </div>
   
+    <div class="container">
+    <div class="row">
+      <h4 class="header center">Suscripción al boletín</h4>
+      <p class="center">
+        Entérate primero de las novedades, recompensas y noticias que Fora tiene preparado para ti. ¡Es gratis!
+      </p>
+      <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+        <div class="col s6">
+          <label for="nombre">Nombre:</label>
+          <input autofocus type="text" name="nombre" id="nombre" class="validate" maxlength="40" pattern="[A-Za-z]{4-40}" required>
+        </div>
+        <div class="col s6">
+          <label for="apellidos">Apellidos:</label>
+          <input type="text" name="apellidos" id="apellidos" class="validate" maxlength="40" pattern="[A-Za-z]{4-40}" required>
+        </div>
+        <div class="col s12">
+          <label for="email">Correo electrónico:</label>
+          <input type="email" name="email" id="email" class="validate" maxlength="40" pattern="[A-Za-z]{4-40}" required>
+        </div>
+        <div class="col s12 center">
+          <p>
+            <label>
+              <input required type="checkbox" name="aceptar" id="aceptar"/>
+              <span>He leído y comprendido los <a href="#" class="blue-text">términos y condiciones</a> que están descritos de forma explícita en <a href="#" class="blue-text">este enlace</a></span>
+            </label>
+            <br><br>
+            <button type="submit" name="suscribirme" class="btn waves-effect waves-light blue lighten-1">Suscribirme</button>
+            <p><a href="bajaSuscripcion.php" class="red-text">DAR DE BAJA MI SUSCRIPCIÓN</a></p>
+          </p>
+        </div>
   
 </head>
 <body>
