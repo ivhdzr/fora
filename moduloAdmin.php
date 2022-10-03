2f00b073d029a184bf08db158834db36f5eeb555
LuzRamosNava <3521110177@uth.edu.mx>
2022-10-02 19:52:30 -0500

    Update moduloAdmin.php

---
+++ moduloAdmin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <meta name="author" content="Equipo de desarrollo FORA"/>
+  <meta name="description" content="FORA. Soluciones de software eficientes e innovadores.">
+    <title>DModulo de administración</title>
+</head>
+<body>
+    
+</body>
+</html>
